522441c41b4105f7d3cceb212f88c7062d1ca10d
joao-lemarq <joaomarcos@lemarq.com.br>
2023-11-13 15:47:00 -0300

    Alteração

---
+++ exercicio3/script3.js
@@ -0,0 +1,15 @@
+// a) 5 é maior que 20 e também é menor que 2;
+
+console.log((5>20) && (5<2))
+
+// b) 5 é igual a 5 ou é igual a “5”;
+
+console.log((5==2) || (5 =="5"))
+
+// c) negação de (vinte é maior que cinquenta)
+
+console.log(!(20>50))
+
+// d) negação de (vinte é maior que cinquenta OU cinquenta é maior que sessenta)
+
+console.log(!((20>50) || (50>60)))
